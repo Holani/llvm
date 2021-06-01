b0107941399f6ec4fd851bd60b4ead4cce76ba58
Holani <priyaholani@gmail.com>
2021-06-01 16:47:39 -0700

    AddingTestcase 2

---
+++ handout/tests/test2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//test case that checks Array accessed by a constant index
+
+int main() {
+  int numbers[10] = {1,2,3,4,5,6,7,8,9,10};
+
+  printf("%i", numbers[10]);
+  
+  return 0;
+}
+
+
