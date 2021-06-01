39360a6dc08f97497ca19c86b50b6c696a7242d6
Holani <priyaholani@gmail.com>
2021-06-01 14:45:35 -0700

    Working indices + code cleanup

--- handout/Instrument.cpp
+++ handout/Instrument.cpp
@@ -20,96 +20,79 @@ bool Instrument::runOnFunction(Module &M, Function &F) {
 
   for (Function::iterator b = F.begin(), be = F.end(); b != be; b++) {
     for (BasicBlock::iterator i = b->begin(), ie = b->end(); i != ie; i++) {      
-      Instruction *inst = (Instruction*)i;
+    	Instruction *inst = (Instruction*)i;
 
-      /***
-       ** STEP-1: 
-       ** Reimplement the following if-condition statement to check
-       ** for the instructions of interest (array accesses).
-       ***/
-      // errs() << "  Instruction: " << *inst << "\n";
-      for (unsigned num = 0; num < inst->getNumOperands(); num++) 
-      	if (isa<GEPOperator>(inst)) {
+      	/***
+       	** STEP-1: 
+       	** Reimplement the following if-condition statement to check
+       	** for the instructions of interest (array accesses).
+       	***/
+      	errs() << "  Instruction: " << *inst << "\n";
+      
+      	if (GetElementPtrInst *GEPInst = dyn_cast<GetElementPtrInst>(inst))
+	  	{
+			errs() << "GEP instruction:" << *GEPInst << "\n";
 
-	/* Use the following to print the instruction to instrument for debugging purposes. */
-	// errs() << "  Instruction: " << *inst << "\n";
+			/* Use the following to print the instruction to instrument for debugging purposes. */
+			// errs() << "  Instruction: " << *inst << "\n";
 
-	/***
-	 ** STEP-2: Retrieve the size of the array
-	 **/
+			/***
+	 		** STEP-2: Retrieve the size of the array
+	 		**/
 
-	Type *T = cast<PointerType>(cast<GetElementPtrInst>(inst)->getPointerOperandType())->getElementType();
-	int no_of_elements = cast<ArrayType>(T)->getNumElements();
+			Type *T = cast<PointerType>(cast<GetElementPtrInst>(inst)->getPointerOperandType())->getElementType();
+			int no_of_elements = cast<ArrayType>(T)->getNumElements();
 
-	LLVMContext& context = F.getContext();
-	// LLVMContext& context;
-	IRBuilder<> builder(inst);
-	// Type *Ty = cast<ArrayType>(T)->getElementType();
-	// Value *size = dyn_cast<GetElementPtrInst>(inst)->getOperand(1);
-
-	llvm::Type *int_ = llvm::IntegerType::getInt64Ty(context);
-	Constant *size = ConstantInt::get(int_, no_of_elements);
-	// errs() << no_of_elements << "\n" << size << "\n\n";
-
-	// Value *array_size = dyn_cast<Value*>(no_of_elements);
-	// Value *array_size = llvm::getInt32(no_of_elements);
-	// ConstantInt* CI = dyn_cast<ConstantInt>(no_of_elements);
-
+			LLVMContext& context = F.getContext();
+			IRBuilder<> builder(inst);
+			
+			//casting no_of_elements to ConstantInt
+			llvm::Type *int_ = llvm::IntegerType::getInt64Ty(context);
+	        Constant *size = ConstantInt::get(int_, no_of_elements);
+	        // errs() << no_of_elements << "\n" << size << "\n\n";
   
-	/***
-	 ** STEP-3: Retrieve the index of the array element being accessed
-	 **/
-
-	// int index_accessed =  cast<GetElementPtrInst>(inst)->getOperand(3);
-	Value *index = dyn_cast<GetElementPtrInst>(inst)->getOperand(inst->getNumOperands()-2);
-
-	// ConstantInt* CI = dyn_cast<ConstantInt>(index);
-	// int intIndex = CI->getZExtValue();
-
-	printf("%i\n", no_of_elements);
-	// errs() << no_of_elements << "\n";
+			/***
+	 		** STEP-3: Retrieve the index of the array element being accessed
+	 		**/
+			
+			Value *index = dyn_cast<GetElementPtrInst>(inst)->getOperand(inst->getNumOperands()-1);
 
 
-	/***
-	 ** STEP-4: Retrieve the source information of the instruction
-	 **/
+			/***
+	 		** STEP-4: Retrieve the source information of the instruction
+	 		**/
 
-	StringRef fileName = F.getParent()->getName();
-	// Value *fn = cast<PointerType>(cast<Value>(fileName));
+		 	//getting filename
 
-	// Value *fn = builder.CreateGlobalString(fileName, "", 0, F.getParent());
-	Value *fn = builder.CreateGlobalStringPtr(fileName);
-	// Value *fn = builder.CreateGEP(cast<PointerType>(fileName));
+			StringRef fileName = F.getParent()->getName();
+			Value *fn = builder.CreateGlobalStringPtr(fileName);
+			// errs() << fileName << " || " << *fn << "\n\n";
 
-	// errs() << fileName << " || " << *fn << "\n\n";
+			const llvm::DebugLoc &debugInfo = inst->getDebugLoc();
 
-	const llvm::DebugLoc &debugInfo = inst->getDebugLoc();
-	int line = debugInfo->getLine();
-	// Type *int_type = Type::getInt8Ty(F.getContext());
-	// llvm::Type *int_ = llvm::IntegerType::getInt64Ty(context);
-	Constant *ln = ConstantInt::get(int_, line);
+			//getting linenumber
+			int line = debugInfo->getLine();
+			Constant *ln = ConstantInt::get(int_, line);
 
-	// errs() << "line: " << ln << "\n\n";
+			// errs() << "line: " << ln << "\n\n";
+	
+			/***
+			 ** STEP-5: Create and store the arguments of function check_bounds
+			** in the vector args.
+			**/
 
+			std::vector<Value*> args = {size, index, ln, fn};
 
-  
-	/***
-	 ** STEP-5: Create and store the arguments of function check_bounds
-	 ** in the vector args.
-	 **/
+			/***
+			 ** The code below creates and inserts the call before inst. Modify as needed.
+			**/
 
-	std::vector<Value*> args = {size, index, ln, fn};
-
-	/***
-	 ** The code below creates and inserts the call before inst. Modify as needed.
-	 **/
-
-	Function *callee = M.getFunction("check_bounds");
-	if (callee) {
-	  CallInst::Create(callee, args, "", inst);
-	}
-      } 
-    }
+			Function *callee = M.getFunction("check_bounds");
+			if (callee) {
+			CallInst::Create(callee, args, "", inst);
+			}
+			} 
+			}
   }
 
   return false;
